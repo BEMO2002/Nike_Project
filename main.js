272367925fab8e34df40a6ee478791b7fd3b3e0a
Mohamed Abdul Azeem <123medoabdo@gmail.com>
2024-10-29 23:28:21 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+let img = document.querySelector(".img");
+let container = document.querySelector(".container");
+function nike(nike){
+    img.src = nike;
+}
+
+function colors(color){
+    container.style.background = color; 
+}
